764ab1945539b8397a4ea7fb02f6b8ca44e2c1dd
Achilleas Koutsou <achilleas.k@gmail.com>
2017-09-28 16:59:17 +0900

    Skipped unsupported or empty properties
    Print read, write, and skip counts when writing has finished

--- convert.py
+++ convert.py
@@ -9,9 +9,41 @@ import nixio as nix
 #   - If a Section has a 'reference' create a property called reference
 #   - If a Property has a 'reference' put the reference in the Property's
 #   values
+#   - Values of type 'URL', 'person', and 'text' are treated as strings
+#   - Values of type 'datetime', 'date', and 'time' are converted to string
+#   representations
+#   - Values of type 'binary' are discarded
+
+
+info = {"sections read": 0,
+        "sections written": 0,
+        "properties read": 0,
+        "properties written": 0,
+        "skipped empty properties": 0,
+        "skipped binary values": 0,
+        "skipped none values": 0,
+        "type errors": 0}
+
+
+def print_info():
+    print("Conversion info")
+    print("{sections read}\t Sections were read\n"
+          "{sections written}\t Sections were written\n"
+          "{properties read}\t Properties were read\n"
+          "{properties written}\t Properties were written\n"
+          "{skipped empty properties}\t Properties were skipped because they "
+          "contained only None or binary values\n"
+          "{skipped binary values}\t Values were skipped because they "
+          "were of type 'binary'\n"
+          "{skipped none values}\t Values were skipped because they were "
+          "empty (None)\n"
+          "{type errors}\t Type Errors were encountered\n".format(**info))
+
 
 def write_recurse(odmlseclist, nixparentsec):
+    global info
     for odmlsec in odmlseclist:
+        info["sections read"] += 1
         secname = odmlsec.name
         type_ = odmlsec.type
         definition = odmlsec.definition
@@ -19,6 +51,7 @@ def write_recurse(odmlseclist, nixparentsec):
         repository = odmlsec.repository
 
         nixsec = nixparentsec.create_section(secname, type_)
+        info["sections written"] += 1
         nixsec.definition = definition
         if reference is not None:
             nixsec["reference"] = reference
@@ -26,26 +59,55 @@ def write_recurse(odmlseclist, nixparentsec):
             nixsec["repository"] = repository
 
         for odmlprop in odmlsec.properties:
+            info["properties read"] += 1
             propname = odmlprop.name
             definition = odmlprop.definition
             odmlvalue = odmlprop.value
-            # dtype = value.dtype
             if isinstance(odmlvalue, Iterable):
                 nixvalues = []
                 for v in odmlvalue:
-                    nixv = nix.Value(v.data)
+                    if v.dtype == "binary":
+                        info["skipped binary values"] += 1
+                        continue
+                    if v.data is None:
+                        info["skipped none values"] += 1
+                        continue
+                    try:
+                        nixv = nix.Value(v.data)
+                    except TypeError as exc:
+                        print("Unsuported data type: {}".format(type(v.data)))
+                        info["type errors"] += 1
+                        continue
                     nixv.unit = v.unit
                     nixv.uncertainty = v.uncertainty
                     nixv.reference = v.reference
                     nixvalues.append(nixv)
             else:
-                nixv = nix.Value(odmlvalue.data)
+                if odmlvalue.dtype == "binary":
+                    info["skipped binary values"] += 1
+                    info["skipped empty properties"] += 1
+                    continue
+                if odmlvalue.data is None:
+                    info["skipped none values"] += 1
+                    info["skipped empty properties"] += 1
+                    continue
+                try:
+                    nixv = nix.Value(odmlvalue.data)
+                except TypeError as exc:
+                    print("Unsuported data type: {}".format(
+                        type(odmlvalue.data)
+                    ))
+                    info["type errors"] += 1
+                    continue
                 nixv.unit = odmlvalue.unit
                 nixv.uncertainty = odmlvalue.uncertainty
                 nixv.reference = odmlvalue.reference
                 nixvalues = [nixv]
 
+            if not nixvalues:
+                info["skipped empty properties"] += 1
             nixprop = nixsec.create_property(propname, nixvalues)
+            info["properties written"] += 1
             nixprop.definition = definition
 
         write_recurse(odmlsec.sections, nixsec)
@@ -76,3 +138,5 @@ if __name__ == "__main__":
     files = sys.argv[1:]
     for f in files:
         main(f)
+
+    print_info()
