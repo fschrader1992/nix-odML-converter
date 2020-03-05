7a240ae1427dd134546eba1fcca11199a9cceeb9
M. Sonntag <michael.p.sonntag@gmail.com>
2020-03-05 10:51:38 +0100

    [init] Update python version notice test

--- nixodmlconverter/__init__.py
+++ nixodmlconverter/__init__.py
@@ -6,7 +6,7 @@ from .info import VERSION
 
 if _python_version.major < 3 or _python_version.major == 3 and _python_version.minor < 6:
     msg = "The '%s' package is not tested with your Python version. " % __name__
-    msg += "Please consider upgrading to the latest Python distribution."
+    msg += "Please consider upgrading to a Python version >= 3.6."
     warnings.warn(msg)
 
 __version__ = VERSION
